ac8306107ba216613646be77b6845b6c4ab99af4
LucasAdao <l.adao@outlook.com>
2025-10-20 16:24:35 -0300

    [fix] Correção JPA Repository

--- src/main/java/dev/lucas/gatosAPI/repository/GatoRepository.java
+++ src/main/java/dev/lucas/gatosAPI/repository/GatoRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface GatoRepository extends JpaRepository<Short, Gato> {
+public interface GatoRepository extends JpaRepository<Gato, Short> {
 }
