0b9be7451f2a69d766836f60d082496737e1537d
LucasAdao <l.adao@outlook.com>
2025-10-20 08:28:57 -0300

    [fix]: Refatoração no nome da DTO PostGato, name -> nome, já que estamos utilizando PT-BR

--- src/main/java/dev/lucas/gatosAPI/dtos/PostGato.java
+++ src/main/java/dev/lucas/gatosAPI/dtos/PostGato.java
@@ -1,6 +1,6 @@
 package dev.lucas.gatosAPI.dtos;
 
-public record PostGato(String name,
+public record PostGato(String nome,
                        String raca,
                        byte idade) {
 }
