bf9d057a8c36525045a144d20d8bca2008bc9828
LucasAdao <l.adao@outlook.com>
2025-10-23 11:27:12 -0300

    [feat]: Adição do handleGatoNotNullException e correção do status da handleGatoNotFoundException

--- src/main/java/dev/lucas/gatosAPI/infra/GlobalExceptionHandler.java
+++ src/main/java/dev/lucas/gatosAPI/infra/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package dev.lucas.gatosAPI.infra;
 
 import dev.lucas.gatosAPI.exceptions.GatoNotFoundException;
+import dev.lucas.gatosAPI.exceptions.GatoNotNullException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
@@ -16,9 +17,18 @@ public class GlobalExceptionHandler {
     @ExceptionHandler({ GatoNotFoundException.class })
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ProblemDetail handleGatoNotFoundException(HttpServletRequest request, GatoNotFoundException e){
-        var pd = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST , e.getMessage());
+        var pd = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND , e.getMessage());
         pd.setTitle("Gato não encontrado!");
         pd.setType(URI.create("https:/www.someurl.com/erros/not-found"));
+        return pd;
+    }
+
+    @ExceptionHandler({ GatoNotNullException.class })
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ProblemDetail handleGatoNotNullException(HttpServletRequest request, GatoNotNullException e) {
+        var pd = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, e.getMessage());
+        pd.setTitle("Gato não preenchido");
+        pd.setType(URI.create("https:/www.someurl.com/erros/bad-request"));
         return pd;
     }
 
