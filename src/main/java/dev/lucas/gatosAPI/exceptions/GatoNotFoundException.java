9eab8b162eafbbb1f3386066e7d6ff0b810305c1
LucasAdao <l.adao@outlook.com>
2025-10-27 04:20:33 -0300

    [feat]: GatoNotNullExcepetion, Exceção para barrar o envio de gatos nulos nos posts ou patchs

--- src/main/java/dev/lucas/gatosAPI/exceptions/GatoNotFoundException.java
+++ src/main/java/dev/lucas/gatosAPI/exceptions/GatoNotFoundException.java
@@ -3,7 +3,7 @@ package dev.lucas.gatosAPI.exceptions;
 public class GatoNotFoundException extends RuntimeException {
 
     public GatoNotFoundException(Short id){
-        super("O gato com ID:" + id + " não está cadastrado");
+        super("O gato com (ID): " + id + " não está cadastrado");
     }
 
 }
